b79dcc2850d79ed8a3dc530121f8f7b3ee47a54c
shujaagreen56 <shujaagreen@gmail.com>
2023-02-07 17:40:46 -0600

    trying to get help on timer again

--- script.js
+++ script.js
@@ -4,28 +4,36 @@ var timeEl = document.querySelector(".time");
 // Selects element by id
 var mainEl = document.getElementById("main");
 var secondsLeft = document.getElementById('countdown');
-
 var second = 18 
+//const minute = second * 60; 
+//const timeOut = minute * 5;
+//const timeRemaining = new Number(0)
+//let timerId
+//const minute = second * 60; 
+//const timeOut = minute * 5;
+//const timeRemaining = new Number(0)
+//let timerId
 //const minute = second * 60; 
 //const timeOut = minute * 5;
 
 //const timeRemaining = new Number(0)
 
 //let timerId 
-console.log(secondsLeft); 
+
+
 
 function countDown(){
 //const timeLeft = 0; 
 second -=1 
 //const timeDone = timeOut - timeRemaining
 console.log(second)
-secondsLeft.innerHTML = second 
-console.log(secondsLeft); 
+secondsLeft.innerHTML = seconds
+console.log(secondsLeft)
 
 if(second = 0){
-    secondsLeft.innerHTML = 'Times up!';
-      clearInterval(this.interval);
-}
+    secondsLeft.innerHTML = 'Times up!'
+      clearInterval()
+console.log(secondsLeft.innerHTML)}}
 
 //if (timeDone <= 0 ){
   //  countDown.innerHTML = "End of Quiz"
@@ -35,9 +43,9 @@ if(second = 0){
 //const minutes = Math.floor(minute); 
 //const seconds = Math.floor(timeDone % minute) / second ; 
 //countDown.innerHTML = minutes + 'minutes' + seconds + 'seconds'
- }
  
- const setInterval = secondsLeft.innerHTML(countDown, 1000) 
+ 
+ var setInterval = countDown.innerHTML(countDown, 1000) 
 
 
 //function windDown(){
