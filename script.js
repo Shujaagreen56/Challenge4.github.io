fdcb55541558a5957b98c1364526f1419508e460
shujaagreen56 <shujaagreen@gmail.com>
2023-02-06 18:25:39 -0600

    take 5

---
+++ script.js
@@ -0,0 +1,52 @@
+// Selects element by class
+
+var timeEl = document.querySelector(".time");
+// Selects element by id
+var mainEl = document.getElementById("main");
+var secondsLeft = document.getElementById('countdown');
+
+var second = 18 
+//const minute = second * 60; 
+//const timeOut = minute * 5;
+
+//const timeRemaining = new Number(0)
+
+//let timerId 
+console.log(secondsLeft.innerHTML)
+
+function countDown(){
+//const timeLeft = 0; 
+second -=1 
+//const timeDone = timeOut - timeRemaining
+console.log(second)
+secondsLeft.innerHTML = second 
+console.log(secondsLeft.innerHTML)
+if(second = 0){
+    secondsLeft.innerHTML = 'Times up!'
+      clearInterval(setInterval)
+
+}
+
+//if (timeDone <= 0 ){
+  //  countDown.innerHTML = "End of Quiz"
+   // clearInterval(timerId)
+  //  return }
+
+//const minutes = Math.floor(minute); 
+//const seconds = Math.floor(timeDone % minute) / second ; 
+//countDown.innerHTML = minutes + 'minutes' + seconds + 'seconds'
+ }
+ 
+ const timer = setInterval(countDown, 1000)
+
+ 
+
+
+//function windDown(){
+  //  alert('Times up!') }
+ //setTimeout(windDown, 50000) 
+
+
+
+
+
